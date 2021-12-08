b922fa15f1dc6545fb7ebf428c3fee34b32dbad4
Ivan Ristović <ivo.ristovic@gmail.com>
2021-12-08 17:11:16 +0100

    [Project] Init

---
+++ XF.NET/XF.NET/GuestXenForoClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.NET
+{
+    public sealed class GuestXenForoClient : XenForoClient
+    {
+        public GuestXenForoClient(string apiKey) : base(apiKey)
+        {
+        }
+    }
+}
