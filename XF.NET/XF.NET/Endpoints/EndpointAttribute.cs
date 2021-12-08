0647bab1809fb5f76056e16a478dbfc6d1bb26e8
Ivan Ristović <ivo.ristovic@gmail.com>
2021-12-08 17:11:24 +0100

    [Endpoints] Init

---
+++ XF.NET/XF.NET/Endpoints/EndpointAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace XF.NET.Endpoints
+{
+    public abstract class Endpoint
+    {
+        internal string Name { get; }
+        internal string ApiKey { get; }
+        internal HttpClient Client { get; }
+
+
+        internal Endpoint(string name, HttpClient client, string apiKey)
+        {
+            this.Name = name;
+            this.Client = client;
+            this.ApiKey = apiKey;
+        }
+    }
+}
---
+++ XF.NET/XF.NET/Endpoints/UsersEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.NET.Endpoints;
+
+public sealed class UsersEndpoint : Endpoint
+{
+    internal UsersEndpoint(HttpClient client, string apiKey) : base("users", client, apiKey)
+    {
+    }
+
+
+}
